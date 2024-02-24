49275a761e7eaaf89c9a92287124c07a7ecd8bd1
rk64346 <128319400+rk64346@users.noreply.github.com>
2024-02-24 20:02:57 +0530

    Add files via upload

--- atliq mart queries 2.sql
+++ atliq mart queries 2.sql
@@ -1,15 +1,7 @@
 SELECT * FROM retail_events_db.fact_events;
-
-
-
- SELECT distinct p.product_name,f.product_code,f.promo_type
- from dim_products as p
- inner join fact_events as f 
- on p.product_code = f.product_code
- where base_price > 500 and promo_type = " BOGOF"
- order by product_name asc;
  
  
+ ## ad-hoc-buisness request 1
  SELECT p.product_name, f.product_code, f.promo_type
 FROM dim_products AS p
 INNER JOIN fact_events AS f ON p.product_code = f.product_code
